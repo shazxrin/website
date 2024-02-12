062de11eee1e9da1e44a6f49844d2b8a5527fd6e
shazrin <mshazrinbs@gmail.com>
2024-02-13 00:01:44 +0800

    Add root error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react"
 import tailwind from "~/styles/tailwind.css"
 import fonts from "~/styles/fonts.css"
@@ -29,6 +31,42 @@ export default function App() {
       <body className="w-screen h-screen bg-black">
         <Outlet />
         <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
+
+export const ErrorBoundary: React.FC = () => {
+  const error = useRouteError()
+
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="w-screen h-screen bg-black">
+        <div className="h-screen w-screen bg flex flex-col justify-center items-center cursor-default">
+          {
+            isRouteErrorResponse(error)
+              ? (
+                <>
+                  <h1 className="text-6xl font-bold text-slate-300">{error.status}</h1>
+                  <p className="text-slate-400">{error.statusText}</p>
+                </>
+              )
+              : (
+                <>
+                  <h1 className="text-6xl font-bold text-slate-300">Unknown Error</h1>
+                </>
+              )
+          }
+        </div>
         <Scripts />
         <LiveReload />
       </body>
