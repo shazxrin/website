704150abb12669c0c972d951a26cf11b2c341f98
shazrin <mshazrinbs@gmail.com>
2024-02-14 13:45:29 +0800

    Update about page

--- app/routes/_main.about.tsx
+++ app/routes/_main.about.tsx
@@ -1,28 +1,35 @@
 import React from "react"
 import AnimatedPage from "~/components/animation/animated-page"
 
+
+
+const Highlighted: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <span className="underline font-semibold">
+      {children}
+    </span>
+  )
+}
+
 const AboutPage: React.FC = () => {
   return (
     <AnimatedPage>
-      <div className="w-full h-full overflow-x-hidden">
-        <div className="flex flex-col w-full px-8 pt-40 pb-12 lg:px-24 cursor-default items-center">
+      <div className="flex flex-col w-full h-full overflow-x-hidden items-center">
+        <div className="w-full sm:max-w-2xl px-8 pt-40 pb-12 lg:px-24 cursor-default">
           <img
             src="/about.webp"
             alt="Younger me with computer"
-            className="w-full sm:max-w-xl h-80 rounded-md object-cover mb-8"
+            className="w-full h-80 rounded-md object-cover mb-8"
           />
-          <div className="w-full sm:max-w-xl text-slate-300 flex flex-col space-y-2">
+          <div className="w-full text-slate-300 flex flex-col space-y-2">
             <p>
-              I am a final year Computer Science student at National University of Singapore with a strong passion for full-stack development. I also have a strong interest in AI and Computer Graphics.
+              I am 24 years old from Singapore. I am currently a final year <Highlighted>Computer Science</Highlighted> student at <Highlighted>National University of Singapore (NUS)</Highlighted>. I am passionate about <Highlighted>Full-stack Web Development</Highlighted> and have a keen interest in Machine Learning and Computer Graphics.
             </p>
             <p>
-              My preferred backend stack is Spring Boot with Kotlin or just plain Go, Docker and Kubernetes for containerization and orchestration, and PostgreSQL for database management.
+              My preferred backend stack is <Highlighted>Spring Boot</Highlighted> with <Highlighted>Kotlin</Highlighted> or <Highlighted>Go</Highlighted>, <Highlighted>Docker</Highlighted> for containerization and <Highlighted>PostgreSQL</Highlighted> for database management. As for the frontend, my preferred stack is using <Highlighted>React</Highlighted> with <Highlighted>TypeScript</Highlighted> built with <Highlighted>Remix</Highlighted> meta-framework for Server Side Rendering (SSR) or <Highlighted>Vite</Highlighted> to create Single Page Application (SPA) that will be bundled together with the backend stack.
             </p>
             <p>
-              As for the frontend, my preferred stack is using React with TypeScript using Next.js/Remix meta-frameworks or just Vite for SPA applications.
-            </p>
-            <p>
-              I enjoy tinkering with new technologies and building cool stuff. Outside of programming, I enjoy playing video games, watching anime, and reading manga.
+              I enjoy tinkering with new technologies, building cool projects and maintaining my home lab during my free time. Outside of programming, I am an avid enjoyer of video games, anime, and manga.
             </p>
           </div>
         </div>
