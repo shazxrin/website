13fe576247bf51cd49e127cedd645daa957e1e0b
shazrin <mshazrinbs@gmail.com>
2024-06-06 17:40:14 +0800

    Update intro

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node"
+import React from "react"
 
 const meta: MetaFunction = () => {
     return [
@@ -13,45 +14,47 @@ type ExternalLinkProps = {
 }
 const ExternalLink = ({ name, link }: ExternalLinkProps) => {
     return (
-        <a href={ link } className={ "text-sm text-gray-500 hover:text-neutral-400 underline underline-offset-4 font-medium" }>
+        <a href={ link }
+           className={ "text-sm text-gray-500 hover:text-neutral-400 underline underline-offset-4 font-medium" }>
             { name }
         </a>
+    )
+}
+
+const Highlight = ({ children }: React.PropsWithChildren) => {
+    return (
+        <span className={ "italic" }>
+           { children }
+        </span>
     )
 }
 
 const Index = () => {
     return (
         <section>
-            <div className={ "flex flex-row items-center" }>
-                <div className={ "mr-auto" }>
-                    <h1 className={ "text-2xl text-neutral-700 font-black" }>Hello, I am Shazrin — </h1>
-                    <h2 className={ "text-xl text-neutral-600 font-bold" }>Software Engineer from Singapore.</h2>
-                    <ul className={ "flex flex-row space-x-5 mt-2" }>
-                        <li>
-                            <ExternalLink name={ "GitHub" } link={ "https://www.github.com/shazxrin" }/>
-                        </li>
-                        <li>
-                            <ExternalLink name={ "LinkedIn" } link={ "https://www.linkedin.com/in/muhammad-shazrin" }/>
-                        </li>
-                        <li>
-                            <ExternalLink name={ "Resume" } link={ "/resume.pdf" }/>
-                        </li>
-                    </ul>
-                </div>
-
-                <img src={ "/notme.jpg" } className={ "w-24 h-24 rounded-full object-cover object-center grayscale" } />
+            <div>
+                <h1 className={ "text-2xl text-neutral-700 font-black" }>Hello, I am Shazrin — </h1>
+                <h2 className={ "text-xl text-neutral-600 font-bold" }>Software Engineer from Singapore.</h2>
+                <ul className={ "flex flex-row space-x-5 mt-2" }>
+                    <li>
+                        <ExternalLink name={ "GitHub" } link={ "https://www.github.com/shazxrin" }/>
+                    </li>
+                    <li>
+                        <ExternalLink name={ "LinkedIn" } link={ "https://www.linkedin.com/in/muhammad-shazrin" }/>
+                    </li>
+                    <li>
+                        <ExternalLink name={ "Resume" } link={ "/resume.pdf" }/>
+                    </li>
+                </ul>
             </div>
 
 
             <p className={ "text-neutral-700 mt-6" }>
-                Recent Computer Science graduate from National University of Singapore. Demonstrated expertise and
-                proficiency in
-                frontend and backend Web Development; with experience in React, in-depth background in Java and its Java
-                Virtual
-                Machine, and professional experience with the Spring ecosystem. Possess knowledge and exposure in DevOps
-                with tools
-                such as Linux, Docker and Kubernetes. Passionate in AI/Machine Learning specifically in Natural Language
-                Processing.
+                I enjoy building things for the web, creating interactive and unique experiences.
+                My preferred tech stack is <Highlight>Spring</Highlight> for the backend and <Highlight>React</Highlight> for the frontend.
+                I am interested in JVM languages such as <Highlight>Java</Highlight> and <Highlight>Kotlin</Highlight>. I also have keen interest in
+                AI/Machine Learning especially NLP and LLMs and have been experimenting with them.
+                I am also dabbling around crafting native <Highlight>Android</Highlight> apps.
             </p>
 
             <hr className={ "h-px my-8 bg-neutral-300 border-0" }/>
